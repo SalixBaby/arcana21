b38919ae39e8e92c66b9050715a052b1189389d1
SalixBaby <hendrik.schmuelling@googlemail.com>
2025-09-04 20:51:53 +0200

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,14 +15,10 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          ♡ in Ahaus
         </p>
         <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
+          © Copyright 2025 | SalixBaby
         </ul>
       </footer>
     )
