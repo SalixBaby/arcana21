17582474b38dd2c61d3b6c43d3a1b6b923f5f4aa
SalixBaby <hendrik.schmuelling@googlemail.com>
2025-09-21 15:56:43 +0200

    Try out

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,13 +27,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
     Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.DesktopOnly(Component.Backlinks()),
-    Component.MobileOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
 }
 
