fb73be4628a139cc1c78aadd603f180c92626b68
SalixBaby <hendrik.schmuelling@googlemail.com>
2025-09-30 11:06:07 +0200

    try somthing out²

--- quartz/components/scripts/explorer.inline.ts
+++ quartz/components/scripts/explorer.inline.ts
@@ -20,7 +20,7 @@ const observer = new IntersectionObserver((entries) => {
 function toggleExplorer(this: HTMLElement) {
   // Toggle collapsed state of entire explorer
   this.classList.toggle("collapsed")
-  window.scrollTo(0, 0)
+  window.scrollTo({ top: 0, left: 0, behavior: "auto" })
   // Toggle collapsed aria state of entire explorer
   this.setAttribute(
     "aria-expanded",
