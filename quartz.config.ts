1694eee0819694504b0bce915b3e1f3040ab5eeb
SalixBaby <hendrik.schmuelling@googlemail.com>
2026-05-19 11:19:39 +0200

    Change locale from 'de-DE' to 'en-EN'

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "de-DE",
+    locale: "en-EN",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
